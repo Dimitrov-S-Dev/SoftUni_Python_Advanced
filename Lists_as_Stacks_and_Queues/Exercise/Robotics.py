62f6650a68538c247a04e7cd5b815f12f96b463b
Stan <dimitrov.s.dev@gmail.com>
2023-05-02 18:54:05 +0300

    SoftUni_Python_Advanced

---
+++ Lists_as_Stacks_and_Queues/Exercise/Robotics.py
@@ -0,0 +1,32 @@
+from collections import deque
+from datetime import datetime, timedelta
+
+robots = {
+    r.split("-")[0]: [int(r.split("-")[1]), 0] for r in input().split(";")
+}
+
+factory_time = datetime.strptime(input(), "%H:%M:%S")
+
+products = deque()
+
+while True:
+    product = input()
+
+    if product == "End":
+        break
+    products.append(product)
+
+while products:
+    factory_time += timedelta(seconds=1)
+    product = products.popleft()
+
+    robots = {r: [v[0], v[1] - 1] if v[1] != 0 else v for r, v in robots.items()}
+    free_robots = list(filter(lambda x: x[1][1] == 0, robots.items()))
+
+    if not free_robots:
+        products.append(product)
+        continue
+
+    robots[free_robots[0][0]][1] = free_robots[0][1][0]
+
+    print(f"{free_robots[0][0]} - {product} [{factory_time.strftime('%H:%M:%S')}]")
