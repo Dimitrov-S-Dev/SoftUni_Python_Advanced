65f15f76bdfdbfa306a339eb3bf3856f6ddc226c
Stan <dimitrov.s.dev@gmail.com>
2023-05-07 08:54:02 +0300

    SoftUni_Python_Advanced

--- Tuples_and_Sets/Exercise/Se_of_Elements.py
+++ Tuples_and_Sets/Exercise/Se_of_Elements.py
@@ -1,17 +1,24 @@
+# n, m = [int(x) for x in input().split()]
+#
+# set_n = set()
+# set_m = set()
+#
+# for _ in range(n):
+#     number = int(input())
+#     set_n.add(number)
+#
+# for _ in range(m):
+#     number = int(input())
+#     set_m.add(number)
+#
+# result = set_n.intersection(set_m)
+#
+# for res in result:
+#     print(res)
+
 n, m = [int(x) for x in input().split()]
 
-set_n = set()
-set_m = set()
+set_n = {int(input()) for _ in range(n)}
+set_m = {int(input()) for _ in range(m)}
+print(*set_n.intersection(set_m), sep="\n")
 
-for _ in range(n):
-    number = int(input())
-    set_n.add(number)
-
-for _ in range(m):
-    number = int(input())
-    set_m.add(number)
-
-result = set_n.intersection(set_m)
-
-for res in result:
-    print(res)
