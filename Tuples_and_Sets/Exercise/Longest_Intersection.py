321460bf5449be9781b03e29a094995c5af40e27
Stan <dimitrov.s.dev@gmail.com>
2023-05-07 10:41:13 +0300

    SoftUni_Python_Advanced

---
+++ Tuples_and_Sets/Exercise/Longest_Intersection.py
@@ -0,0 +1,14 @@
+longest_intersection = set()
+
+for _ in range(int(input())):
+    first_data, second_data = [elem.split(",") for elem in input().split("-")]
+    f_set = set(x for x in range(int(first_data[0]), int(first_data[1] + 1)))
+    s_set = set(x for x in range(int(second_data[0]), int(second_data[1] + 1)))
+    intersection = f_set.intersection(s_set)
+
+    if len(intersection) > len(longest_intersection):
+        longest_intersection = intersection
+
+print(f"Longest intersection is"
+      f"[{','.join(str(x) for x in longest_intersection)}]"
+      f"with length {len(longest_intersection)}")
