c11a690c2f3b10d7a0d26668f9688cd5c2261075
Stan <dimitrov.s.dev@gmail.com>
2023-05-09 19:12:24 +0300

    SoftUni_Python_Advanced

--- Lists_as_Stacks_and_Queues/Exercise/Fashion_Boutique.py
+++ Lists_as_Stacks_and_Queues/Exercise/Fashion_Boutique.py
@@ -1,15 +1,33 @@
-clothes = [int(n) for n in input().split()]
-rack_space = int(input())
+# clothes = [int(n) for n in input().split()]
+# rack_space = int(input())
+#
+# rack_count = 1
+# current_rack_space = rack_space
+#
+# while clothes:
+#     cloth = clothes.pop()
+#
+#     if current_rack_space - cloth >= 0:
+#         current_rack_space -= cloth
+#     else:
+#         rack_count += 1
+#         current_rack_space = rack_space - cloth
+# print(rack_count)
 
-rack_count = 1
-current_rack_space = rack_space
+clothes = [int(x) for x in input().split()]
+rack_capacity = int(input())
+
+curr_rack_capacity = rack_capacity
+rack_count = 0
 
 while clothes:
-    cloth = clothes.pop()
+    cloth = clothes[-1]
 
-    if current_rack_space - cloth >= 0:
-        current_rack_space -= cloth
-    else:
+    if cloth > curr_rack_capacity:
         rack_count += 1
-        current_rack_space = rack_space - cloth
+        curr_rack_capacity = rack_capacity
+    else:
+        curr_rack_capacity -= cloth
+        clothes.pop()
+
 print(rack_count)
