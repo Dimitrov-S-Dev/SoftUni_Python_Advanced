689105b7a95eacc538ba37709d997c085f4e47ca
Stan <dimitrov.s.dev@gmail.com>
2023-05-09 22:18:45 +0300

    SoftUni_Python_Advanced

--- Lists_as_Stacks_and_Queues/Exercise/Truck_Tour.py
+++ Lists_as_Stacks_and_Queues/Exercise/Truck_Tour.py
@@ -1,21 +1,38 @@
 from collections import deque
+#
+# pumps_data = deque([[int(x) for x in input().split()] for _ in range(int(input()))])
+#
+# pumps_data_copy = pumps_data.copy()
+# index = 0
+# gas_in_tank = 0
+#
+# while pumps_data_copy:
+#     petrol, distance = pumps_data_copy.popleft()
+#
+#     gas_in_tank += petrol
+#     if gas_in_tank - distance < 0:
+#         pumps_data.rotate(-1)
+#         pumps_data_copy = pumps_data.copy()
+#         index += 1
+#         gas_in_tank = 0
+#     else:
+#         gas_in_tank -= distance
+#
+# print(index)
+pumps_count = int(input())
+pumps = deque([int(x) for x in input().split()]for _ in range(pumps_count))
 
-pumps_data = deque([[int(x) for x in input().split()] for _ in range(int(input()))])
+for attempt in range(pumps_count):
+    trunk = 0
+    failed = False
+    for petrol, distance in pumps:
+        trunk += petrol + trunk - distance
 
-pumps_data_copy = pumps_data.copy()
-index = 0
-gas_in_tank = 0
-
-while pumps_data_copy:
-    petrol, distance = pumps_data_copy.popleft()
-
-    gas_in_tank += petrol
-    if gas_in_tank - distance < 0:
-        pumps_data.rotate(-1)
-        pumps_data_copy = pumps_data.copy()
-        index += 1
-        gas_in_tank = 0
+        if trunk < 0:
+            flag = True
+            break
+    if failed:
+        pumps.append(pumps.popleft())
     else:
-        gas_in_tank -= distance
-
-print(index)
+        print(attempt)
+        break
