b6edf0fcafe51563daa8da5957a6b3b350d0f080
Stan <dimitrov.s.dev@gmail.com>
2023-05-10 13:19:42 +0300

    SoftUni_Python_Advanced

--- Tuples_and_Sets/Lab/Count_Same_Values.py
+++ Tuples_and_Sets/Lab/Count_Same_Values.py
@@ -1,10 +1,15 @@
-values = tuple(map(float, input().split()))
-value_counter = {}
+# values = tuple(map(float, input().split()))
+# value_counter = {}
+#
+# for value in values:
+#     if value not in value_counter:
+#         value_counter[value] = 0
+#     value_counter[value] += 1
+#
+# for k, v in value_counter.items():
+#     print(f"{k} - {v} times")
 
-for value in values:
-    if value not in value_counter:
-        value_counter[value] = 0
-    value_counter[value] += 1
-
-for k, v in value_counter.items():
+numbers = [float(x) for x in input().split()]
+numbers_dict = {x: numbers.count(x) for x in numbers}
+for k, v in numbers_dict.items():
     print(f"{k} - {v} times")
