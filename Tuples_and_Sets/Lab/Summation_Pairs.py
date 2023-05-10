053735597a769504313f0e1de18312de291100fb
Stan <dimitrov.s.dev@gmail.com>
2023-05-10 14:40:44 +0300

    SoftUni_Python_Advanced

--- Tuples_and_Sets/Lab/Summation_Pairs.py
+++ Tuples_and_Sets/Lab/Summation_Pairs.py
@@ -1,10 +1,24 @@
-import itertools
+# import itertools
+#
+# numbers = [int(x) for x in input().split()]
+# target = int(input())
+#
+# result = set([x for x in itertools.combinations(numbers, r=2) if x[0] + x[1] == target])
+#
+# for res in result:
+#     print(f"{res[0]} + {res[1]} = {target}")
 
-numbers = [int(x) for x in input().split()]
-target = int(input())
+numbers = [1, 3, 2, 4, 5, -2, -1, 5, 3, -3, 7, 0]
 
-result = set([x for x in itertools.combinations(numbers, r=2) if x[0] + x[1] == target])
+target = 4
+values_map = {}
+targets = set()
 
-for res in result:
-    print(f"{res[0]} + {res[1]} = {target}")
-
+for value in numbers:
+    if value in targets:
+        p1 = value
+        p2 = values_map[value]
+        print(f"{p1} + {p2} = {target}")
+    else:
+        targets.add(target - value)
+        values_map[target - value] = value
