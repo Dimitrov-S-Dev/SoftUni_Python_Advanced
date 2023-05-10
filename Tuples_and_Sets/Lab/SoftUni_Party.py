60ea6f279b7921f65e168cded5da7f64838b6058
Stan <dimitrov.s.dev@gmail.com>
2023-05-10 13:57:18 +0300

    SoftUni_Python_Advanced

--- Tuples_and_Sets/Lab/SoftUni_Party.py
+++ Tuples_and_Sets/Lab/SoftUni_Party.py
@@ -1,24 +1,50 @@
-def get_guests():
-    guests = []
-    while True:
-        data = input()
-        if data == "End":
-            break
-        guests.append(data)
-    return guests
+# def get_guests():
+#     guests = []
+#     while True:
+#         data = input()
+#         if data == "End":
+#             break
+#         guests.append(data)
+#     return guests
+#
+#
+# def print_func(not_arrived_data):
+#     print(len(not_arrived_data))
+#     for guest_num in sorted(not_arrived_data):
+#         print(guest_num)
+#
+#
+# n = int(input())
+# reservation_codes = [input() for _ in range(n)]
+# arrived_guests = get_guests()
+# not_arrived_guests = set(reservation_codes).difference(arrived_guests)
+# print_func(not_arrived_guests)
 
-
-def print_func(not_arrived_data):
-    print(len(not_arrived_data))
-    for guest_num in sorted(not_arrived_data):
-        print(guest_num)
+def is_vip(guest):
+    return guest[0].isdigit()
 
 
 n = int(input())
-reservation_codes = [input() for _ in range(n)]
-arrived_guests = get_guests()
-not_arrived_guests = set(reservation_codes).difference(arrived_guests)
-print_func(not_arrived_guests)
 
+vip_guests = set()
+regular_guests = set()
 
+for _ in range(n):
+    reservation = input()
+    if is_vip(reservation):
+        vip_guests.add(reservation)
+    else:
+        regular_guests.add(reservation)
 
+while True:
+    guest = input()
+    if guest == "END":
+        break
+    if is_vip(guest):
+        vip_guests.remove(guest)
+    else:
+        regular_guests.remove(guest)
+
+print(len(vip_guests) + len(regular_guests))
+[print(guest) for guest in sorted(vip_guests)]
+[print(guest) for guest in sorted(regular_guests)]
