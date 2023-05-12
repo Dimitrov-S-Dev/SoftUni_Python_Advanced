8534e999181334bcd103fd8a23274ec5e954d3ae
Stan <dimitrov.s.dev@gmail.com>
2023-05-12 07:21:44 +0300

    SoftUni_Python_Advanced

--- Multidimensional_Lists/Lab/Symbol_in_Matrix.py
+++ Multidimensional_Lists/Lab/Symbol_in_Matrix.py
@@ -1,18 +1,19 @@
-n = int(input())
+def find_symbol(matrix, symbol):
+    for row_index in range(n):
+        for column_index in range(n):
+            if matrix[row_index][column_index] == sbl:
+                return row_index, column_index
+    return None
 
+
+n = int(input())
 matrix = [list(input()) for _ in range(n)]
 
 sbl = input()
-is_found = False
+result = find_symbol(matrix, sbl)
 
-for row_index in range(n):
-    if is_found:
-        break
-    for column_index in range(n):
-        if matrix[row_index][column_index] == sbl:
-            is_found = True
-            print(f"({row_index}, {column_index})")
-            break
-
-if not is_found:
+if result:
+    row_index, column_index = result
+    print(f"({row_index}, {column_index})")
+else:
     print(f"{sbl} does not occur in the matrix")
