1be321ac6d76fe2386dedf027269a65b1c174e0a
Stan <dimitrov.s.dev@gmail.com>
2023-05-12 07:15:23 +0300

    SoftUni_Python_Advanced

--- Multidimensional_Lists/Lab/Symbol_in_Matrix.py
+++ Multidimensional_Lists/Lab/Symbol_in_Matrix.py
@@ -4,11 +4,15 @@ matrix = [list(input()) for _ in range(n)]
 
 sbl = input()
 is_found = False
+
 for row_index in range(n):
+    if is_found:
+        break
     for column_index in range(n):
         if matrix[row_index][column_index] == sbl:
             is_found = True
             print(f"({row_index}, {column_index})")
+            break
 
 if not is_found:
     print(f"{sbl} does not occur in the matrix")
