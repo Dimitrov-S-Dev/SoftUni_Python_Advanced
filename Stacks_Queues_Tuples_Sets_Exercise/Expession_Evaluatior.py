4c36bd830749f1838ad2e7e953e74ea1bcf4ff01
Stan <dimitrov.s.dev@gmail.com>
2023-05-16 07:58:23 +0300

    SoftUni_Python_Advanced

--- Stacks_Queues_Tuples_Sets_Exercise/Expession_Evaluatior.py
+++ Stacks_Queues_Tuples_Sets_Exercise/Expession_Evaluatior.py
@@ -1,25 +1,59 @@
-from functools import reduce
+# from functools import reduce
+#
+#
+# expression = input().split()
+#
+# index = 0
+#
+# functions = {
+#     "*": lambda i: reduce(lambda a, b: int(a) * int(b), expression[:i]),
+#     "/": lambda i: reduce(lambda a, b: int(a) / int(b), expression[:i]),
+#     "-": lambda i: reduce(lambda a, b: int(a) - int(b), expression[:i]),
+#     "+": lambda i: reduce(lambda a, b: int(a) + int(b), expression[:i]),
+# }
+#
+# while index < len(expression):
+#     element = expression[index]
+#
+#     if element in "*/+-":
+#         result = functions[element](index)
+#         [expression.pop(1) for i in range(index)]
+#         expression[0] = result
+#         index = 0
+#     index += 1
+#
+# print(int(expression[0]))
 
+from collections import deque
 
-expression = input().split()
+expression = deque(input().split())
 
-index = 0
+idx = 0
 
-functions = {
-    "*": lambda i: reduce(lambda a, b: int(a) * int(b), expression[:i]),
-    "/": lambda i: reduce(lambda a, b: int(a) / int(b), expression[:i]),
-    "-": lambda i: reduce(lambda a, b: int(a) - int(b), expression[:i]),
-    "+": lambda i: reduce(lambda a, b: int(a) + int(b), expression[:i]),
-}
+while idx < len(expression):
+    element = expression[idx]
 
-while index < len(expression):
-    element = expression[index]
+    if element == "*":
+        for _ in range(idx - 1):
+            expression.appendleft(int(expression.popleft()) * int(expression.popleft()))
 
-    if element in "*/+-":
-        result = functions[element](index)
-        [expression.pop(1) for i in range(index)]
-        expression[0] = result
-        index = 0
-    index += 1
+    elif element == "+":
+        for _ in range(idx - 1):
+            expression.appendleft(int(expression.popleft()) + int(expression.popleft()))
+
+    elif element == "-":
+        for _ in range(idx - 1):
+            expression.appendleft(int(expression.popleft()) - int(expression.popleft()))
+
+    elif element == "/":
+        for _ in range(idx - 1):
+            expression.appendleft(int(expression.popleft()) / int(expression.popleft()))
+
+    if element in "+-*/":
+        del expression[1]
+        idx = 0
+
+    idx += 1
+
 
 print(int(expression[0]))
