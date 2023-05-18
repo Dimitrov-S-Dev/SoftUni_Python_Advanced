3edb1ab7440cab5f34373e257398c5827b06570e
Stan <dimitrov.s.dev@gmail.com>
2023-05-18 13:10:51 +0300

    SoftUni_Python_Advanced

--- Multidimensional_Lists/Exercise/First/Matrix+of_Polindromes.py
+++ Multidimensional_Lists/Exercise/First/Matrix+of_Polindromes.py
@@ -1,2 +1,9 @@
-row, column = [int(x) for x in input().split()]
+rows, cols = [int(x) for x in input().split()]
+
+start_end = ord("a")
+
+for row in range(start_end, start_end + rows):
+    for col in range(start_end, start_end + cols):
+        print(f"{chr(row)}{chr(row + col - start_end)}{chr(row)}", end=" ")
+
 
