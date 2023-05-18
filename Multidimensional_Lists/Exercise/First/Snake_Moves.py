77a42b766faa2dbe851f1ace80ee57481d01aa3a
Stan <dimitrov.s.dev@gmail.com>
2023-05-18 10:23:37 +0300

    SoftUni_Python_Advanced

---
+++ Multidimensional_Lists/Exercise/First/Snake_Moves.py
@@ -0,0 +1,15 @@
+rows, columns = [int(x) for x in input().split()]
+word = input()
+index = 0
+
+for row in range(rows):
+    element = ["None"] * columns
+    if row % 2 == 0:
+        for col in range(columns):
+            element[col] = word[index % len(word)]
+            index += 1
+    else:
+        for col in range(columns - 1, -1, -1):
+            element[col] = word[index % len(word)]
+            index += 1
+    print("".join(element))
