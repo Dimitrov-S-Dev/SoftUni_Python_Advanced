f66de1338d1220cfccfc6c29e266202c40a38218
Stan <dimitrov.s.dev@gmail.com>
2023-05-18 07:37:48 +0300

    SoftUni_Python_Advanced

--- Multidimensional_Lists/Exercise/First/2x2_Squares_in_Matrix.py
+++ Multidimensional_Lists/Exercise/First/2x2_Squares_in_Matrix.py
@@ -6,9 +6,10 @@ for _ in range(row):
     row_data = [x for x in input().split()]
     matrix.append(row_data)
 
-for i in range(row - 1):
-    for j in range(column - 1):
-        if matrix[i][j] == matrix[i][j + 1] == matrix[i + 1][j] == matrix[i + 1][j + 1]:
+for row in range(row - 1):
+    for col in range(column - 1):
+        if matrix[row][col] == matrix[row][col + 1] == matrix[row + 1][col] \
+                == matrix[row + 1][col + 1]:
             squares += 1
 
 print(squares)
