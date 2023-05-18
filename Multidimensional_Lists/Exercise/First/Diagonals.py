66946474bc24db582f4574a5842a37d8bb582b36
Stan <dimitrov.s.dev@gmail.com>
2023-05-18 07:10:12 +0300

    SoftUni_Python_Advanced

--- Multidimensional_Lists/Exercise/First/Diagonals.py
+++ Multidimensional_Lists/Exercise/First/Diagonals.py
@@ -1,3 +1,24 @@
+# size = int(input())
+# matrix = []
+# p_diagonal = []
+# s_diagonal = []
+#
+# for _ in range(size):
+#     curr_row = [int(x) for x in input().split(", ")]
+#     matrix.append(curr_row)
+#
+# for i in range(size):
+#     for j in range(size):
+#         if i == j:
+#             p_diagonal.append(matrix[i][j])
+#         if i + j == size - 1:
+#             s_diagonal.append(matrix[i][j])
+#
+# print(f"Primary diagonal: {', '.join(str(x) for x in p_diagonal)}. "
+#       f"Sum: {sum(p_diagonal)}")
+# print(f"Secondary diagonal: {', '.join(str(x) for x in s_diagonal)}. "
+#       f"Sum: {sum(s_diagonal)}")
+
 size = int(input())
 matrix = []
 p_diagonal = []
@@ -8,14 +29,12 @@ for _ in range(size):
     matrix.append(curr_row)
 
 for i in range(size):
-    for j in range(size):
-        if i == j:
-            p_diagonal.append(matrix[i][j])
-        if i + j == size - 1:
-            s_diagonal.append(matrix[i][j])
+    p_diagonal.append(matrix[i][i])
+    s_diagonal.append(matrix[i][size - 1 - i])
 
 print(f"Primary diagonal: {', '.join(str(x) for x in p_diagonal)}. "
       f"Sum: {sum(p_diagonal)}")
 print(f"Secondary diagonal: {', '.join(str(x) for x in s_diagonal)}. "
       f"Sum: {sum(s_diagonal)}")
+
 
