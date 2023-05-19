60057d109b9d3b3cc79530b85542ff881da549a5
Stan <dimitrov.s.dev@gmail.com>
2023-05-19 15:26:59 +0300

    SoftUni_Python_Advanced

--- Multidimensional_Lists/Exercise/First/Matrix+of_Polindromes.py
+++ Multidimensional_Lists/Exercise/First/Matrix+of_Polindromes.py
@@ -5,5 +5,6 @@ start_end = ord("a")
 for row in range(start_end, start_end + rows):
     for col in range(start_end, start_end + cols):
         print(f"{chr(row)}{chr(row + col - start_end)}{chr(row)}", end=" ")
+    print()
 
 
