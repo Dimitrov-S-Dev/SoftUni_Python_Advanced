3887d7ca694f721b9b7c83394af437abeb485fba
Stan <dimitrov.s.dev@gmail.com>
2023-05-23 16:13:05 +0300

    SoftUni_Python_Advanced

---
+++ Functions_Advanced/Exercise/Concatenate.py
@@ -0,0 +1,10 @@
+def concatenate(*args, **kwargs):
+    result = ""
+    for arg in args:
+        result += arg
+    for k, v in kwargs.items():
+        if k in result:
+            result = result.replace(k, v)
+
+    return result
+
