4fdd8104fd9529aa1215c7f64cbda5c036fd1da3
Stan <dimitrov.s.dev@gmail.com>
2023-05-23 17:11:50 +0300

    SoftUni_Python_Advanced

--- Functions_Advanced/Exercise/Grocery.py
+++ Functions_Advanced/Exercise/Grocery.py
@@ -1,7 +1,7 @@
 def grocery_store(**kwargs):
     result = ""
-    sorted_dict = dict(sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0])))
-    for k, v in sorted_dict.items():
-        result += f"{k}: {v}\n"
+    sorted_product = sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+    for product, quantity in sorted_product:
+        result += f"{product}: {quantity}\n"
 
     return result
