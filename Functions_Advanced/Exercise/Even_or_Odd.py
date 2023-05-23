b65bd5406cb0e67909353c05e697315e1e68d47c
Stan <dimitrov.s.dev@gmail.com>
2023-05-23 15:19:15 +0300

    SoftUni_Python_Advanced

---
+++ Functions_Advanced/Exercise/Even_or_Odd.py
@@ -0,0 +1,6 @@
+def even_odd(*args):
+    command = args[-1]
+    if command == "even":
+        return [x for x in args[:-1] if x % 2 == 0]
+    else:
+        return [x for x in args[:-1] if x % 2 != 0]
