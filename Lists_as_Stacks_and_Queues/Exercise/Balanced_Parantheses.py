1df1201cdeef1fd431b725381cba02a1d3b89b84
Stan <dimitrov.s.dev@gmail.com>
2023-05-24 21:45:27 +0300

    SoftUni_Python_Advanced

--- Lists_as_Stacks_and_Queues/Exercise/Balanced_Parantheses.py
+++ Lists_as_Stacks_and_Queues/Exercise/Balanced_Parantheses.py
@@ -1,50 +1,48 @@
 from collections import deque
 
-# parentheses = deque()
-# open_parentheses = deque()
-#
-# while parentheses:
-#     left_parentheses = parentheses.popleft()
-#
-#     if left_parentheses in "({[":
-#         open_parentheses.append(left_parentheses)
-#
-#     elif not open_parentheses:
-#         print("NO")
-#         break
-#     else:
-#         if f"{open_parentheses.pop() + left_parentheses}" not in "[]{}()":
-#             print("NO")
-#             break
-# else:
-#     print("YES")
+parentheses = deque(list(input()))
+open_parentheses = deque()
 
-expression = input()
-opening_brackets = deque()
+while parentheses:
+    left_parentheses = parentheses.popleft()
 
-pairs = {
-    "(": ")",
-    "{": "}",
-    "[": "]"
-}
-balanced = True
+    if left_parentheses in "({[":
+        open_parentheses.append(left_parentheses)
 
-for elem in expression:
-    if elem in "{[(":
-        opening_brackets.append(elem)
-    elif not opening_brackets:
-        balanced = False
-    else:
-        last_open_bracket = opening_brackets.pop()
-        if pairs[last_open_bracket] != elem:
-            balanced = False
-
-    if not balanced:
+    elif not open_parentheses:
+        print("NO")
         break
-if not balanced or opening_brackets:
-    print("NO")
+    else:
+        if f"{open_parentheses.pop() + left_parentheses}" not in "[]{}()":
+            print("NO")
+            break
 else:
     print("YES")
 
+# expression = input()
+# opening_brackets = deque()
 
-
+# pairs = {
+#     "(": ")",
+#     "{": "}",
+#     "[": "]"
+# }
+# balanced = True
+#
+# for elem in expression:
+#     if elem in "{[(":
+#         opening_brackets.append(elem)
+#     elif not opening_brackets:
+#         balanced = False
+#     else:
+#         last_open_bracket = opening_brackets.pop()
+#         if pairs[last_open_bracket] != elem:
+#             balanced = False
+#
+#     if not balanced:
+#         break
+# if not balanced or opening_brackets:
+#     print("NO")
+# else:
+#     print("YES")
+#
