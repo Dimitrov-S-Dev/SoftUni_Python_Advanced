97619916e7f57525dd682e057126620ddc5ad7de
Stan <dimitrov.s.dev@gmail.com>
2023-05-25 03:51:50 +0300

    SoftUni_Python_Advanced

--- Error_Handling/Lab/So_many_Exceptions.py
+++ Error_Handling/Lab/So_many_Exceptions.py
@@ -16,7 +16,7 @@ result = 1
 for number in numbers_list:
     if number <= 5:
         result *= number
-    elif 5 < number <= 10:
+    elif number <= 10:
         result /= number
 
 print(result)
