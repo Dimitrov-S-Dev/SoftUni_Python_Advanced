ab6a8b3973fc66935aa6de4a6ee249b1abaab2c8
Stan <dimitrov.s.dev@gmail.com>
2023-05-25 04:30:03 +0300

    SoftUni_Python_Advanced

---
+++ Error_Handling/Lab/Repeat_Text.py
@@ -0,0 +1,8 @@
+try:
+    text = input()
+    times = int(input())
+    output = text * times
+    print(output)
+except ValueError:
+    print("Variable times must be an integer!")
+    print("What the heck?")
