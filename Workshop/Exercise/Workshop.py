756fa344f779b04f4a23a942b3295d5e60c2d503
Stan <dimitrov.s.dev@gmail.com>
2023-05-31 17:57:03 +0300

    SoftUni_Python_Advanced

---
+++ Workshop/Exercise/Workshop.py
@@ -0,0 +1,59 @@
+def check_for_win():
+    player_name, player_symbol = players[0]
+    size = len(board)
+
+    first_diagonal = all([board[i][i] == player_symbol for i in range(size)])
+
+
+def place_symbol(position):
+    row, col = (position - 1) // 3, (position - 1) % 3
+
+    board[row][col] = players[0][1]
+    print_board(begin=False)
+
+
+def choose_position():
+    while True:
+        position = input(f"{players[0][0]} choose a free position [1-9]: ")
+
+        if any([True if position in row else False for row in board]):
+            break
+        else:
+            print(f"{players[0][0]} please select a valid position!")
+
+    place_symbol(int(position))
+
+
+def print_board(begin=False):
+    if begin:
+        print(f"This is the numeration of the board:")
+        [print(f"| {' | '.join(row)} |") for row in board]
+        print(f"{players[0][0]} starts first!")
+    else:
+        [print(f"| {' | '.join(row)} |") for row in board]
+
+
+def start():
+    player_one_name = input("Player one name: ")
+    player_two_name = input("Player two name: ")
+
+    while True:
+        player_one_symbol = input(f"{player_one_name} "
+                                  f"would you like to play with X or 0").upper()
+        if player_one_symbol not in "XO":
+            print(f"{player_one_name} please select a valid option")
+        else:
+            break
+
+    player_two_symbol = "O" if player_one_symbol == "X" else "X"
+
+    players.append([player_one_name, player_one_symbol])
+    players.append([player_two_name, player_two_symbol])
+
+    print_board(begin=True)
+    choose_position()
+
+
+players = []
+board = [[str(i), str(i + 1), str(i + 2)]for i in range(1, 10, 3)]
+start()
