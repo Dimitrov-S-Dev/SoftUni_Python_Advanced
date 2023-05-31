77ffd3d89f3ebe62ad6079f3bb5e459ddbf31a9c
Stan <dimitrov.s.dev@gmail.com>
2023-05-31 18:18:47 +0300

    SoftUni_Python_Advanced

--- Workshop/Exercise/Workshop.py
+++ Workshop/Exercise/Workshop.py
@@ -7,12 +7,22 @@ def check_for_win():
     row_win = any([all(True if pos == player_symbol else False for pos in row)for row in board])
     col_win = any([all(True if board[r][c] == player_symbol else False for r in range(size))for c in range(size)])
 
+    if any([first_diagonal_win, second_diagonal_win, row_win, col_win]):
+        print_board()
+        print(f"{player_name} won!")
+
+        raise SystemExit
+    players.append(players.pop())
+    choose_position()
+
 
 def place_symbol(position):
     row, col = (position - 1) // 3, (position - 1) % 3
 
     board[row][col] = players[0][1]
-    print_board(begin=False)
+    check_for_win()
+    print_board()
+    choose_position()
 
 
 def choose_position():
