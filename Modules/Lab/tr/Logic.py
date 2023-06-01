11fa6492910b7c6937b227f686202323c2a74d0f
Stan <dimitrov.s.dev@gmail.com>
2023-06-01 23:29:07 +0300

    SoftUni_Python_Advanced

--- Modules/Lab/tr/Logic.py
+++ Modules/Lab/tr/Logic.py
@@ -1,11 +1,10 @@
+def print_line(nums):
+    print(*[num for num in range(1, nums + 1)], sep=" ")
+
+
 def print_triangle(size):
     for row in range(1, size + 1):
-        for col in range(1, row + 1):
-            print(col, end=" ")
-        print()
+        print_line(row)
 
     for row in range(size - 1, 0, -1):
-        for col in range(1, row + 1):
-            print(col, end=" ")
-        print()
-
+        print_line(row)
