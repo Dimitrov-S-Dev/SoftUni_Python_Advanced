e045ccc921eca927ae4a099e626b6bb0bb070752
Stan <dimitrov.s.dev@gmail.com>
2023-06-02 00:03:15 +0300

    SoftUni_Python_Advanced

---
+++ Modules/Lab/Mathematical_Operations.py
@@ -0,0 +1,5 @@
+from tr.Math import math_expression
+first_num, operator, second_num = input().split()
+
+print(f"{math_expression(float(first_num), operator, float(second_num)):.2f}")
+
---
+++ Modules/Lab/tr/Math.py
@@ -0,0 +1,34 @@
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+def power(num1, num2):
+    return num1 ** num2
+
+
+def math_expression(num1, operator, num2):
+    result = None
+    if operator == "+":
+        result = add(num1, num2)
+    elif operator == "-":
+        result = subtract(num1, num2)
+    elif operator == "*":
+        result = multiply(num1, num2)
+    elif operator == "/":
+        result = divide(num1, num2)
+    elif operator == "^":
+        result = power(num1, num2)
+
+    return result
