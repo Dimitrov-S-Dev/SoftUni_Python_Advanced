358292c0c8759c8107b3da3c0b6f2d11640431ec
Stan <dimitrov.s.dev@gmail.com>
2023-06-07 15:17:37 +0300

    SoftUni_Python_Advanced

--- Exams_Prep/December_2022/Climb_the_Peaks.py
+++ Exams_Prep/December_2022/Climb_the_Peaks.py
@@ -1,38 +1,46 @@
+# Step 1: Initialize the food and stamina from input
 from collections import deque
 
-# read the input data
 food = deque(int(x) for x in input().split(", "))
 stamina = deque(int(x) for x in input().split(", "))
-# Initializing the Peaks and Difficulty
-peaks = deque([
-    ["Vihren", 80],
-    ["Kutelo", 90],
-    ["Banski Suhodol", 100],
-    ["Polezhan", 60],
-    ["Kamenitza", 70],
-])
+
+
+# Step 2: Create peaks with difficulty level
+
+peaks = {
+    80: "Vihren",
+    90: "Kutelo",
+    100: "Banski Suhodol",
+    60: "Polezhan",
+    70: "Kamenitza",
+
+}
+
+# Step 3: Initialize the output variables
+
 conquered_peaks = []
-failed_condition = False
-day = 1
 
-while True:
-    if len(conquered_peaks) == 5:
-        print(f"Alex did it! He climbed all top five Pirin peaks "
-              f"in one week -> @FIVEinAWEEK")
-        break
-    if not food or day > 7:
-        print(f"Alex failed! He has to organize his "
-              "journey better next time -> @PIRINWINS")
-        break
+# Step 4: Main Loop
 
-    daily_sum = food.pop() + stamina.popleft()
-    name, value = peaks.popleft()
-    if daily_sum >= value:
-        conquered_peaks.append(name)
-    else:
-        peaks.appendleft([name, value])
-    day += 1
+for level in peaks.keys():
+    while food:
+        curr_food = food.pop()
+        curr_stamina = stamina.popleft()
+        curr_sum = curr_food + curr_stamina
+        if curr_sum >= level:
+            conquered_peaks.append(peaks[level])
+            break
+        else:
+            continue
+
+if len(conquered_peaks) == len(peaks):
+    print("Alex did it! He climbed all top five Pirin peaks in one week -> "
+          "@FIVEinAWEEK")
+else:
+    print("Alex failed! He has to organize his journey better next time -> "
+          "@PIRINWINS")
 
 if conquered_peaks:
     print("Conquered peaks:")
-    [print(x) for x in conquered_peaks]
+    for conquered in conquered_peaks:
+        print(conquered)
