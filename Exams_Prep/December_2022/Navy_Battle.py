4d12fdb21e26b2cd90ee4c61cbc6fe6c161cf9b3
Stan <dimitrov.s.dev@gmail.com>
2023-06-07 15:38:58 +0300

    SoftUni_Python_Advanced

--- Exams_Prep/December_2022/Navy_Battle.py
+++ Exams_Prep/December_2022/Navy_Battle.py
@@ -1,60 +1,55 @@
-# Read the matrix size
-n = int(input())
+# Step 1: Initialize the battlefield from input
+size = int(input())
 matrix = []
 
-# Create matrix
-for _ in range(n):
-    data_row = list(input())
-    matrix.append(data_row)
+for _ in range(size):
+    curr_row = list(input())
+    matrix.append(curr_row)
 
-# Locate the submarine, cruisers  and mines
-submarine_location = (0, 0)
-mines_locations = []
+start_position = (0, 0)
+# Step 2: Find Submarine location
 
-for i in range(n):
-    for j in range(n):
+for i in range(size):
+    for j in range(size):
         if matrix[i][j] == "S":
-            submarine_location = i, j
-        elif matrix[i][j] == "*":
-            mines_locations.append((i, j))
+            start_position = i, j
 
-# Initialize the movements
+# Step 3: Initialize the movements
 
 movements = {
-    "up": lambda r, c: (r - 1, c),
-    "down": lambda r, c: (r + 1, c),
-    "left": lambda r, c: (r, c - 1),
-    "right": lambda r, c: (r, c + 1),
+    "up": lambda x, y: (x - 1, y),
+    "down": lambda x, y: (x + 1, y),
+    "left": lambda x, y: (x, y - 1),
+    "right": lambda x, y: (x, y + 1),
 }
 
-start_r, start_c = submarine_location
-matrix[start_r][start_c] = "-"
-battle_cruisers = 3
-submarine_hits = 0
-# Main Loop
+# Step 4: Main Loop
+start_row, start_col = start_position
+matrix[start_row][start_col] = "-"
+mine_count = 0
+cruiser_count = 0
+
 while True:
     command = input()
-    new_r, new_c = movements[command](start_r, start_c)
-    start_r, start_c = new_r, new_c
+    new_row, new_col = movements[command](start_row, start_col)
 
-    if matrix[new_r][new_c] == "*":
-        submarine_hits += 1
-        if submarine_hits == 3:
-            matrix[new_r][new_c] = "S"
-            print(f"Mission failed, U-9 disappeared! "
-                  f"Last known coordinates [{new_r}, {new_c}]!")
+    if matrix[new_row][new_col] == "*":
+        mine_count += 1
+        if mine_count == 3:
+            print("Mission failed, U-9 disappeared! Last known coordinates "
+                  f"[{new_row}, {new_col}]!")
             break
-        else:
-            matrix[new_r][new_c] = "-"
+        matrix[new_row][new_col] = "-"
 
-    elif matrix[new_r][new_c] == "C":
-        matrix[new_r][new_c] = "-"
-        battle_cruisers -= 1
-        if battle_cruisers == 0:
-            matrix[new_r][new_c] = "S"
+    elif matrix[new_row][new_col] == "C":
+        cruiser_count += 1
+        if cruiser_count == 3:
             print("Mission accomplished, U-9 has destroyed all battle cruisers"
                   " of the enemy!")
             break
+        matrix[new_row][new_col] = "-"
 
+    start_row, start_col = new_row, new_col
 
-[print("".join(row)) for row in matrix]
+matrix[new_row][new_col] = "S"
+[print(''.join(row))for row in matrix]
