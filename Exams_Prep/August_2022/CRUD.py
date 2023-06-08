154ced299eb40bd6f948b3bee9a563825880a65a
Stan <dimitrov.s.dev@gmail.com>
2023-06-08 15:20:54 +0300

    SoftUni_Python_Advanced

--- Exams_Prep/August_2022/CRUD.py
+++ Exams_Prep/August_2022/CRUD.py
@@ -1,43 +1,51 @@
-# Initialize the matrix 6 X 6
-SIZE = 6
+# Stage 4: Create direction function
+def get_direction(matrix, row, col, sub_command, value):
+    if sub_command == "Create":
+        if matrix[new_row][new_col] == ".":
+            matrix[new_row][new_col] = value[1]
+
+    elif sub_command == "Update":
+        if matrix[row][col].isalnum():
+            matrix[row][col] = value[1]
+
+    elif sub_command == "Delete":
+        if matrix[row][col].isalnum():
+            matrix[row][col] = "."
+
+    elif sub_command == "Read":
+        if matrix[row][col].isalnum():
+            print(matrix[row][col])
+
+    return matrix
+
+
+# Stage 1: Initialize matrix and start position from input
+size = 6
 matrix = []
 
-for _ in range(SIZE):
-    row_data = input().split()
-    matrix.append(row_data)
+for _ in range(size):
+    curr_row = input().split()
+    matrix.append(curr_row)
 
-starting_position = input()
-start_row, start_col = int(starting_position[1]), int(starting_position[4])
+# Stage 2: Get the starting position from input
+start_row, start_col = [int(x) for x in input().strip("()").split(", ")]
 
-# Initialize Directions
-
+# Stage 3: Initialize directions
 directions = {
-    "up": lambda row, col: (row - 1, col),
-    "down": lambda row, col: (row + 1, col),
-    "left": lambda row, col: (row, col - 1),
-    "right": lambda row, col: (row, col + 1),
+        "up": lambda x, y: (x - 1, y),
+        "down": lambda x, y: (x + 1, y),
+        "left": lambda x, y: (x, y - 1),
+        "right": lambda x, y: (x, y + 1),
 }
-# Main Loop
+# Stage 4: Main Loop
 
 while True:
-    command = input()
+    command, *args = input().split(", ")
     if command == "Stop":
         break
-    sub_command = command.split(", ")
+    new_row, new_col = directions[args[0]](start_row, start_col)
+    start_row, start_col = new_row, new_col
 
-    new_r, new_c = directions[sub_command[1]](start_row, start_col)
-    start_row, start_col = new_r, new_c
-
-    if sub_command[0] == "Create" and matrix[new_r][new_c] == ".":
-        matrix[new_r][new_c] = sub_command[2]
-
-    elif sub_command[0] == "Update" and matrix[new_r][new_c].isalnum():
-        matrix[new_r][new_c] = sub_command[2]
-
-    elif sub_command[0] == "Delete" and matrix[new_r][new_c].isalnum():
-        matrix[new_r][new_c] = "."
-
-    elif sub_command[0] == "Read" and matrix[new_r][new_c].isalnum():
-        print(matrix[new_r][new_c])
+    matrix = get_direction(matrix, new_row, new_col, command,args)
 
 [print(" ".join(row)) for row in matrix]
