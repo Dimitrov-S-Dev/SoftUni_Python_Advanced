8f04534b4cd94a29c8451d74d9b6404a991c0a5b
Stan <dimitrov.s.dev@gmail.com>
2023-06-12 12:43:36 +0300

    SoftUni_Python_Advanced

--- Exams_Prep/June_2022/Exit_Founder.py
+++ Exams_Prep/June_2022/Exit_Founder.py
@@ -1,61 +1,36 @@
-# Initialize a function to check the position
 from collections import deque
 
-
-def get_position_check(matrix, row, col):
-    if matrix[row][col] == "E":
-        return "win"
-    elif matrix[row][col] == "T":
-        return "lose"
-    elif matrix[row][col] == "W":
-        return "rest"
-
-
-# Initialize matrix 6 x 6 and players
 players = deque(input().split(", "))
 matrix = []
 
 for _ in range(6):
-    row_data = input().split()
-    matrix.append(row_data)
+    curr_row = input().split()
+    matrix.append(curr_row)
 
-# Main Loop
-
-turn_count = 0
-even_rest = False
-odd_rest = False
+rest = {
+    "Tom": "No",
+    "Jerry": "No",
+}
 while True:
-    turn_count += 1
-    curr_row, curr_col = [int(x) for x in input().strip("()").split(", ")]
+    coordinates = list(input())
+    row, col = int(coordinates[1]), int(coordinates[4])
+    curr_player = players[0]
 
-    if turn_count % 2 != 0:
-        if odd_rest:
-            odd_rest = False
-            players.rotate(-1)
-            continue
-    else:
-        if even_rest:
-            even_rest = False
-            players.rotate(- 1)
-            continue
+    if rest[curr_player] == "Yes":
+        players.rotate(-1)
+        rest[curr_player] = "No"
+        continue
 
-    player = players.popleft()
-
-    result = get_position_check(matrix, curr_row, curr_col)
-
-    if result == "win":
-        print(f"{player} found the Exit and wins the game!")
+    if matrix[row][col] == "E":
+        print(f"{curr_player} found the Exit and wins the game!")
         break
 
-    elif result == "lose":
-        print(f"{player} is out of the game! The winner is {players.pop()}.")
+    elif matrix[row][col] == "T":
+        print(f"{curr_player} is out of the game! The winner is {players[1]}.")
         break
 
-    elif result == "rest":
-        print(f"{player} hits a wall and needs to rest.")
-        if turn_count % 2 != 0:
-            odd_rest = True
-        else:
-            even_rest = True
+    elif matrix[row][col] == "W":
+        print(f"{curr_player} hits a wall and needs to rest.")
+        rest[curr_player] = "Yes"
 
-    players.append(player)
+    players.rotate(-1)
