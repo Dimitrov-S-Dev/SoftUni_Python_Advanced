8756d6d3498e82cc67468af7c512dbca90a533ad
Stan <dimitrov.s.dev@gmail.com>
2023-06-16 01:51:59 +0300

    SoftUni_Python_Advanced

--- Exams_Prep/October_2020/Scheduling.py
+++ Exams_Prep/October_2020/Scheduling.py
@@ -1,3 +1,16 @@
+from collections import deque
 
+jobs = deque(int(x) for x in input().split(', '))
+target_index = int(input())
+
+target_job = jobs[target_index]
+cycles = 0
+
+for i in range(len(jobs)):
+    current_job = jobs[i]
+    if current_job < target_job or current_job == target_job and target_index >= i:
+        cycles += current_job
+
+print(cycles)
 
 
