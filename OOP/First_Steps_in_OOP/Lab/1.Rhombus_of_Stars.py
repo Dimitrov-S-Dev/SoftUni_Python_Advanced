2a1e58838f72f9bb724dd48dc590571fe2a0485f
Stan <dimitrov.s.dev@gmail.com>
2023-06-18 01:07:09 +0300

    SoftUni_Python_Advanced

---
+++ OOP/First_Steps_in_OOP/Lab/1.Rhombus_of_Stars.py
@@ -0,0 +1,26 @@
+def print_row(figure_size, row_size):
+    print(f"{' ' * (figure_size - row_size)}", end="")
+    for _ in range(1, row_size + 1):
+        print("* ", end="")
+    print()
+
+
+n = int(input())
+
+# for row in range(1, n + 1):
+#     print(" " * (n - row), end="")
+#     for col in range(1, row + 1):
+#         print("* ", end="")
+#     print()
+#
+# for row in range(n - 1, -1, -1):
+#     print(" " * (n - row), end="")
+#     for col in range(1, row + 1):
+#         print("* ", end="")
+#     print()
+
+for row in range(1, n + 1):
+    print_row(n, row)
+
+for row in range(n - 1, - 1, -1):
+    print_row(n, row)
