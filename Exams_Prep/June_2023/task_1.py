5aff1107425cd451060d278d107ae4cc10bc7491
Stan <dimitrov.s.dev@gmail.com>
2023-06-17 13:37:01 +0300

    SoftUni_Python_Advanced

---
+++ Exams_Prep/June_2023/task_1.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+x = deque(int(x) for x in input().split())
+y = deque(int(x) for x in input().split())
+
+
+while x and y:
+    pass
+
+
+
+
+
+
+
+if x:
+    print()
+else:
+    print()
+
+if y:
+    print()
+else:
+    print()
