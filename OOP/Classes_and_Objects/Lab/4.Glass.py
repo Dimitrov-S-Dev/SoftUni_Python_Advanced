7969963233357eda21f0d3bb0c36b2ede5b76dc8
Stan <dimitrov.s.dev@gmail.com>
2023-06-24 04:08:56 +0300

    SoftUni_Python_Advanced

--- OOP/Classes_and_Objects/Lab/4.Glass.py
+++ OOP/Classes_and_Objects/Lab/4.Glass.py
@@ -1,12 +1,14 @@
 class Glass:
+    capacity = 250
+
     def __init__(self):
         self.content = 0
-        self.capacity = 250
 
     def fill(self, ml):
-        if self.content + ml <= self.capacity:
+        if self.content + ml <= Glass.capacity:
             self.content += ml
             return f"Glass filled with {ml} ml"
+
         return f"Cannot add {ml} ml"
 
     def empty(self):
@@ -14,6 +16,6 @@ class Glass:
         return "Glass is now empty"
 
     def info(self):
-        space_left = self.capacity - self.content
+        space_left = Glass.capacity - self.content
         return f"{space_left} ml left"
 
