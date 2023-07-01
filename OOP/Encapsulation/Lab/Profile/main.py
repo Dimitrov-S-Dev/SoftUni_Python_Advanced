7811bcc4c7feb57f52dcd02ec12affd20f221e0c
Stan <dimitrov.s.dev@gmail.com>
2023-07-01 11:17:48 +0300

    SoftUni_Python_Advanced

--- OOP/Encapsulation/Lab/Profile/main.py
+++ OOP/Encapsulation/Lab/Profile/main.py
@@ -1,33 +1,49 @@
 class Profile:
+    min_username_length = 5
+    max_username_length = 15
+
+    min_password_length = 8
+    min_uppercase_letters_count = 1
+    min_digits_count = 1
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def __validate_username(self, username):
+        if len(username) not in range\
+                   (self.min_username_length, self.min_username_length):
+            raise ValueError("The username must be between 5 and 15"
+                             " characters.")
+
+    def __validate_password(self, pass_value):
+        has_uppercase = len([c for c in pass_value if c.isupper()]) \
+                        > self.min_uppercase_letters_count
+        has_digit = len([d for d in pass_value if d.isdigit()]) \
+                        > self.min_digits_count
+        has_min_length = len(pass_value) >= self.min_password_length
+
+        if not has_uppercase or not has_digit or not has_min_length:
+            raise ValueError("The password must be 8 or more characters "
+                             "with at least 1 digit and 1 uppercase letter.")
 
     @property
     def username(self):
         return self.__username
 
     @username.setter
-    def username(self, username_value):
-        if len(username_value) not in range(5, 16):
-            raise ValueError("The username must be between 5 and 15"
-                             " characters.")
-        self.__username = username_value
+    def username(self, value):
+        self.__validate_username(value)
+        self.__username = value
 
     @property
     def password(self):
         return self.__password
 
     @password.setter
-    def password(self, pass_value):
-        has_uppercase = len([c for c in pass_value if c.isupper()]) > 0
-        has_digit = len([d for d in pass_value if d.isdigit()]) > 0
-        has_min_length = len(pass_value) >= 8
-
-        if not has_uppercase or not has_digit or not has_min_length:
-            raise ValueError("The password must be 8 or more characters "
-                             "with at least 1 digit and 1 uppercase letter.")
-        self.__password = pass_value
+    def password(self, value):
+        self.__validate_password(value)
+        self.__password = value
 
     def __str__(self):
         return f'You have a profile with username: "{self.__username}" ' \
