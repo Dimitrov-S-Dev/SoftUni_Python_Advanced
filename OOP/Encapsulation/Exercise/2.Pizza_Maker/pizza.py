50ed66416d4cd084123367647df572b1cea37dd4
Stan <dimitrov.s.dev@gmail.com>
2023-07-05 15:07:15 +0300

    SoftUni_Python_Advanced

--- OOP/Encapsulation/Exercise/2.Pizza_Maker/pizza.py
+++ OOP/Encapsulation/Exercise/2.Pizza_Maker/pizza.py
@@ -1,12 +1,11 @@
-from project.dough import Dough
-from project.topping import Topping
+from topping import Topping
 
 
 class Pizza:
-    def __init__(self, name, dough, topping_capacity):
+    def __init__(self, name, dough, max_number_of_toppings):
         self.name = name
         self.dough = dough
-        self.topping_capacity = topping_capacity
+        self.max_number_of_toppings = max_number_of_toppings
         self.toppings = {}
 
     @property
@@ -30,25 +29,25 @@ class Pizza:
         self.__dough = value
 
     @property
-    def topping_capacity(self):
-        return self.__topping_capacity
+    def max_number_of_toppings(self):
+        return self.__max_number_of_toppings
 
-    @topping_capacity.setter
-    def topping_capacity(self, value):
+    @ max_number_of_toppings.setter
+    def max_number_of_toppings(self, value):
         if value <= 0:
-            raise ValueError("The topping's capacity cannot be less or "
-                             "equal to zero")
-        self.__topping_capacity = value
+            raise ValueError("The maximum number of toppings cannot be "
+                             "less or equal to zero")
+        self.__max_number_of_toppings = value
 
     def add_topping(self, topping: Topping):
-        if self.__topping_capacity == len(self.toppings):
+        if self.max_number_of_toppings == len(self.toppings):
             raise ValueError("Not enough space for another topping")
-        if topping.topping_type is not self.toppings:
+        if topping.topping_type not in self.toppings:
             self.toppings[topping.topping_type] = 0
         self.toppings[topping.topping_type] += topping.weight
 
     def calculate_total_weight(self):
         total_dough = self.dough.weight
-        total_topping = sum(t[1] for t in self.toppings.items())
+        total_topping = sum(self.toppings.values())
 
         return total_dough + total_topping
