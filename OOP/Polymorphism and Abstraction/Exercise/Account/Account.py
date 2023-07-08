892ef96e1e22ff59778b67142f52ab60fe855e34
Stan <dimitrov.s.dev@gmail.com>
2023-07-08 23:39:19 +0300

    SoftUni_Python_Advanced

---
+++ OOP/Polymorphism and Abstraction/Exercise/Account/Account.py
@@ -0,0 +1,12 @@
+class Account:
+    def __init__(self, owner: str, amount: int = 0):
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+        self._transactions.append(amount)
+
+
