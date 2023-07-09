a577ac4668d02c1535721a16e6aa90e99da32e82
Stan <dimitrov.s.dev@gmail.com>
2023-07-09 11:24:43 +0300

    SoftUni_Python_Advanced

--- OOP/Polymorphism and Abstraction/Exercise/Vehicle/Vehicle.py
+++ OOP/Polymorphism and Abstraction/Exercise/Vehicle/Vehicle.py
@@ -7,23 +7,23 @@ class Vehicle(ABC):
         self.fuel_consumption = fuel_consumption
 
     @abstractmethod
-    def drive(self, distance):
+    def drive(self, distance) -> None:
         ...
 
     @abstractmethod
-    def refuel(self, fuel):
+    def refuel(self, fuel) -> None:
         ...
 
 
 class Car(Vehicle):
     AC_FUEL_CONS = 0.9
 
-    def drive(self, distance):
+    def drive(self, distance: float):
         fuel_needed = distance * (self.fuel_consumption + self.AC_FUEL_CONS)
         if self.fuel_quantity >= fuel_needed:
             self.fuel_quantity -= (self.fuel_consumption + 0.9) * distance
 
-    def refuel(self, fuel):
+    def refuel(self, fuel: float) -> None:
         self.fuel_quantity += fuel
 
 
