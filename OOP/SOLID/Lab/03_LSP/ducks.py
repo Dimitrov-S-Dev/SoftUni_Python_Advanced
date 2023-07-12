abae67d9950b4b8803cb54608b7e7372e8805741
Stan <dimitrov.s.dev@gmail.com>
2023-07-12 18:37:42 +0300

    SoftUni_Python_Advanced

--- OOP/SOLID/Lab/03_LSP/ducks.py
+++ OOP/SOLID/Lab/03_LSP/ducks.py
@@ -2,6 +2,57 @@ from abc import abstractmethod, ABC
 
 
 class Duck(ABC):
+    @abstractmethod
+    def quack(self):
+        pass
+
+
+class Walkable(ABC):
+    @abstractmethod
+    def walk(self):
+        pass
+
+
+class Flyable(ABC):
+    @abstractmethod
+    def fly(self):
+        pass
+
+
+class RubberDuck(Duck):
+    def quack(self):
+        return "Squeek"
+
+
+class RobotDuck(Duck, Walkable, Flyable):
+    HEIGHT = 50
+
+    def __init__(self):
+        self.height = 0
+
+    def quack(self):
+        return 'Robotic quacking'
+
+    def walk(self):
+        return 'Robotic walking'
+
+    def fly(self):
+        if self.height == RobotDuck.HEIGHT:
+            self.land()
+        else:
+            self.height += 1
+
+    def land(self):
+        self.height = 0
+
+
+
+"""
+
+from abc import abstractmethod, ABC
+
+
+class Duck(ABC):
     @staticmethod
     def quack():
         pass
@@ -22,13 +73,13 @@ class RubberDuck(Duck):
 
     @staticmethod
     def walk():
-        """Rubber duck can walk only if you move it"""
-        return 'I cannot walk by myself'
+        ---Rubber duck can walk only if you move ---
+        raise Exception('I cannot walk by myself')
 
     @staticmethod
     def fly():
-        """Rubber duck can fly only if you throw it"""
-        return 'I cannot fly by myself'
+        ---Rubber duck can fly only if you throw it---
+        - ('I cannot fly by myself')-
 
 
 class RobotDuck(Duck):
@@ -46,16 +97,13 @@ class RobotDuck(Duck):
         return 'Robotic walking'
 
     def fly(self):
-        """can only fly to specific height but
-        when it reaches it starts landing automatically"""
+        ---can only fly to specific height but
+        when it reaches it starts landing automatically---
         if self.height == RobotDuck.HEIGHT:
             self.land()
         else:
-            self.height += 1
-
+        self.height += 1
     def land(self):
         self.height = 0
 
-
-
-
+"""
