f30d8a89ff5d9b52d15ae9dce5177affec3ab13c
Stan <dimitrov.s.dev@gmail.com>
2023-07-12 06:12:32 +0300

    SoftUni_Python_Advanced

---
+++ OOP/SOLID/Lab/01_SRP/books.py
@@ -0,0 +1,29 @@
+from typing import List, Union
+
+
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+        self.page = 0
+
+    def turn_page(self, page):
+        self.page = page
+
+
+class Library:
+    def __init__(self):
+        self.books: List[Book] = []
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+
+    def find_book(self, book_title) -> Union[Book, str]:
+        try:
+            return [b for b in self.books if b.title == book_title][0]
+        except ValueError:
+            return "Book does not exist"
+
+
+
+
