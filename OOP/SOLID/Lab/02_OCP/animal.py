a1b5abd4e045892fbfbdd14e1c67d7109553ea1c
Stan <dimitrov.s.dev@gmail.com>
2023-07-12 06:41:47 +0300

    SoftUni_Python_Advanced

---
+++ OOP/SOLID/Lab/02_OCP/animal.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        return f"{self.name} is making "
+    
+    
+class Dog(Animal):
+    def make_sound(self):
+        return super().make_sound() + " woof-woof"
+
+
+class Cat(Animal):
+    def make_sound(self):
+        return super().make_sound() + " meow"
+
+
+class Chicken(Animal):
+    def make_sound(self):
+        return super().make_sound() + " cluck"
+
+
+def animal_sound(animals: list):
+    for animal in animals:
+        print(animal.make_sound())
+
+
+animals = [Dog("Sharo"), Cat("Lady")]
+animal_sound(animals)
+
+## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
+## при добавяне на нови животни
+# animals = [Animal('cat'), Animal('dog'), Animal('chicken')]
