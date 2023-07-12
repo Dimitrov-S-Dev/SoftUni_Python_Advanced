84442d2b94213842a61f1e4c6d8d09cdb2bbd872
Stan <dimitrov.s.dev@gmail.com>
2023-07-13 00:22:21 +0300

    SoftUni_Python_Advanced

--- OOP/SOLID/Lab/05_DIP/print_books.py
+++ OOP/SOLID/Lab/05_DIP/print_books.py
@@ -1,7 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class Book:
+    def __init__(self, title, content: str):
+        self.title = title
+        self.content = content
+
+
+class BaseFormatter(ABC):
+    @abstractmethod
+    def format(self, book: Book):
+        pass
+
+
+class WebFormatter(BaseFormatter):
+    def format(self, book: Book):
+        return f"{book.content}"
+
+
+class PaperFormatter(BaseFormatter):
+    def format(self, book: Book) -> str:
+        return f"{book.title}\n{book.content}"
+
+
+class Printer:
+    def __init__(self, formatter: BaseFormatter):
+        self.formatter = formatter
+
+    def get_book(self, book: Book):
+        return self.formatter.format(book)
+
 
 """
-
-
 class Book:
     def __init__(self, content: str):
         self.content = content
