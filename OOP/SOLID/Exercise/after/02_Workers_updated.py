a0a5d19b40043bd8e72fa5dc089638292bbaaaf5
Stan <dimitrov.s.dev@gmail.com>
2023-07-13 18:15:59 +0300

    SoftUni_Python_Advanced

--- OOP/SOLID/Exercise/after/02_Workers_updated.py
+++ OOP/SOLID/Exercise/after/02_Workers_updated.py
@@ -80,16 +80,22 @@ class BreakManager(Manager):
 
 work_manager = WorkManager()
 break_manager = BreakManager()
+
 work_manager.set_worker(Worker())
 break_manager.set_worker(Worker())
+
 work_manager.manage()
 break_manager.lunch_break()
+
 work_manager.set_worker(SuperWorker())
 break_manager.set_worker(SuperWorker())
+
 work_manager.manage()
 break_manager.lunch_break()
+
 work_manager.set_worker(Robot())
 work_manager.manage()
+
 try:
     break_manager.set_worker(Robot())
     break_manager.lunch_break()
