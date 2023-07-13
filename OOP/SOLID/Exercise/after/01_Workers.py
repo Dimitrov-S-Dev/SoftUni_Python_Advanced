e7648a9bbc58b9d1c8b69bdb0f60f45fcf32ebf6
Stan <dimitrov.s.dev@gmail.com>
2023-07-13 18:17:31 +0300

    SoftUni_Python_Advanced

--- OOP/SOLID/Exercise/after/01_Workers.py
+++ OOP/SOLID/Exercise/after/01_Workers.py
@@ -45,10 +45,13 @@ class Manager:
 
 
 worker = Worker()
+
 manager = Manager()
 manager.set_worker(worker)
 manager.manage()
+
 super_worker = SuperWorker()
+
 try:
     manager.set_worker(super_worker)
     manager.manage()
