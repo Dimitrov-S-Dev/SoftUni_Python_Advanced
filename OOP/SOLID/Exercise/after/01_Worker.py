97388718cb3a0331a80c36e89eb5f67a05fd3170
Stan <dimitrov.s.dev@gmail.com>
2023-07-13 17:32:33 +0300

    SoftUni_Python_Advanced

---
+++ OOP/SOLID/Exercise/after/01_Worker.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+
+class BaseWorker(ABC):
+    @staticmethod
+    @abstractmethod
+    def work():
+        pass
+
+
+class Worker(BaseWorker):
+
+    @staticmethod
+    def work():
+        print("I'm working!!")
+
+
+class SuperWorker(BaseWorker):
+
+    @staticmethod
+    def work():
+        print("I work very hard!!!")
+
+
+class MasterWorker(BaseWorker):
+    @staticmethod
+    def work():
+        print("working 24/7")
+
+
+class Manager:
+
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        if not isinstance(worker, BaseWorker):
+            raise AssertionError(f"'worker' must be subclass of {BaseWorker}")
+
+        self.worker = worker
+
+    def manage(self):
+        if self.worker is not None:
+            self.worker.work()
+
+
+worker = Worker()
+manager = Manager()
+manager.set_worker(worker)
+manager.manage()
+
+super_worker = SuperWorker()
+try:
+    manager.set_worker(super_worker)
+    manager.manage()
+except AssertionError:
+    print("manager fails to support super_worker....")
