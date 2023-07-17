d1e0e5485236c112d2e68621f047c78f539dcd1d
Stan <dimitrov.s.dev@gmail.com>
2023-07-17 21:39:15 +0300

    SoftUni_Python_Advanced

---
+++ OOP/Iterators_andGenerators/Exercise/Prime_Numbers.py
@@ -0,0 +1,17 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for number in range(2, num - 1):
+        if num % number == 0:
+            return False
+    return True
+
+
+def get_primes(ll):
+    for num in ll:
+        if is_prime(num):
+            yield num
+
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
+print(list(get_primes([-2, 0, 0, 1, 1, 0])))
