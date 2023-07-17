3726781424eef3472a91e4ce08134371b1d29b55
Stan <dimitrov.s.dev@gmail.com>
2023-07-17 18:41:44 +0300

    SoftUni_Python_Advanced

---
+++ OOP/Iterators_andGenerators/Exercise/Dictionary_Iterator.py
@@ -0,0 +1,16 @@
+class dictionary_iter:
+    def __init__(self, data):
+        self.items = list(data.items())
+        self.idx = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.idx >= len(self.items):
+            raise StopIteration
+
+        result = self.items[self.idx]
+        self.idx += 1
+        return result
+
