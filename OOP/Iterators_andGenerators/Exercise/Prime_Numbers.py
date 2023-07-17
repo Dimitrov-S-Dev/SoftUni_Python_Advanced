336d295765647d159d94266b07e2657e0e6c433e
Stan <dimitrov.s.dev@gmail.com>
2023-07-17 21:41:42 +0300

    SoftUni_Python_Advanced

--- OOP/Iterators_andGenerators/Exercise/Prime_Numbers.py
+++ OOP/Iterators_andGenerators/Exercise/Prime_Numbers.py
@@ -1,7 +1,7 @@
 def is_prime(num):
     if num <= 1:
         return False
-    for number in range(2, num - 1):
+    for number in range(2, num):
         if num % number == 0:
             return False
     return True
