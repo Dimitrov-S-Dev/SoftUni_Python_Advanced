fbc99495ba779b0decbd50978564786bde58b887
Stan <dimitrov.s.dev@gmail.com>
2023-07-17 20:40:46 +0300

    SoftUni_Python_Advanced

---
+++ OOP/Iterators_andGenerators/Exercise/Sequence_Repeat.py
@@ -0,0 +1,15 @@
+class sequence_repeat:
+    def __init__(self, seq, count):
+        self.seq = seq
+        self.count = count
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == self.count:
+            raise StopIteration
+        result = self.seq[self.index % len(self.seq)]
+        self.index += 1
+        return result
