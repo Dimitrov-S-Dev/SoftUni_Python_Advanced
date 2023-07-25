629c68380e7d19008cf39443007dc817d365b6ed
Stan <dimitrov.s.dev@gmail.com>
2023-07-25 14:26:24 +0300

    SoftUni_Python_Advanced

---
+++ OOP/Exam_Prep/Aug_2022/Part_1/Horse_Racings/horse_race_app.py
@@ -0,0 +1,99 @@
+from project.horse_race import HorseRace
+from project.horse_specification.appaloosa import Appaloosa
+from project.horse_specification.thoroughbred import Thoroughbred
+from project.jockey import Jockey
+
+
+class HorseRaceApp:
+    VALID_TYPES = {
+        "Appaloosa": Appaloosa,
+        "Thoroughbred": Thoroughbred,
+    }
+
+    def __init__(self):
+        self.horses = []
+        self.jockeys = []
+        self.horse_races = []
+
+    def add_horse(self, horse_type: str, horse_name: str, horse_speed: int):
+        if horse_name in [h.name for h in self.horses]:
+            raise Exception(f"Horse {horse_name} has been already added!")
+
+        if horse_type in self.VALID_TYPES:
+            horse = self.VALID_TYPES[horse_type](horse_name, horse_speed,)
+            self.horses.append(horse)
+            return f"{horse_type} horse {horse_name} is added."
+
+    def add_jockey(self, jockey_name: str, age: int):
+        if jockey_name in [j.name for j in self.jockeys]:
+            raise Exception(f"Jockey {jockey_name} has been already added!")
+
+        jockey = Jockey(jockey_name, age)
+        self.jockeys.append(jockey)
+        return f"Jockey {jockey_name} is added."
+
+    def create_horse_race(self, race_type: str):
+        if race_type in [r.race_type for r in self.horse_races]:
+            raise Exception(f"Race {race_type} has been already created!")
+
+        race = HorseRace(race_type)
+        self.horse_races.append(race)
+        return f"Race {race_type} is created."
+
+    def add_horse_to_jockey(self, jockey_name: str, horse_type: str):
+        try:
+            jockey = next(filter(lambda j: j.name == jockey_name, self.jockeys))
+        except StopIteration:
+            raise Exception(f"Jockey {jockey_name} could not be found!")
+
+        try:
+            horse = list(filter(lambda h: h.__class__.__name__ == horse_type and not h.is_taken, self.horses))[-1]
+        except IndexError:
+            raise Exception(f"Horse breed {horse_type} could not be found!")
+
+        if jockey.horse:
+            return f"Jockey {jockey_name} already has a horse."
+
+        jockey.horse = horse
+        horse.is_taken = True
+        return f"Jockey {jockey_name} will ride the horse {horse.name}."
+
+    def add_jockey_to_horse_race(self, race_type: str, jockey_name: str):
+        try:
+            race = next(filter(lambda r: r.race_type == race_type, self.horse_races))
+        except StopIteration:
+            raise Exception(f"Race {race_type} could not be found!")
+
+        try:
+            jockey = next(filter(lambda j: j.name == jockey_name, self.jockeys))
+        except StopIteration:
+            raise Exception(f"Jockey {jockey_name} could not be found!")
+
+        if not jockey.horse:
+            raise Exception(f"Jockey {jockey_name} cannot race without a horse!")
+
+        if jockey in race.jockeys:
+            return f"Jockey {jockey_name} has been already added to the {race_type} race."
+
+        race.jockeys.append(jockey)
+        return f"Jockey {jockey_name} added to the {race_type} race."
+
+    def start_horse_race(self, race_type: str):
+        if race_type not in [r.race_type for r in self.horse_races]:
+            raise Exception(f"Race {race_type} could not be found!")
+
+        race = list(filter(lambda r: r.race_type == race_type, self.horse_races))[0]
+
+        if len(race.jockeys) < 2:
+            raise Exception(f"Horse race {race_type} needs at least two participants!")
+
+        winner = None
+        highest_speed = 0
+        horse_name = None
+        for jockey in race.jockeys:
+            if jockey.horse.speed > highest_speed:
+                highest_speed = jockey.horse.speed
+                winner = jockey.name
+                horse_name = jockey.horse.name
+
+        return f"The winner of the {race_type} race, with a speed of {highest_speed}km/h is {winner}! Winner's horse: {horse_name}."
